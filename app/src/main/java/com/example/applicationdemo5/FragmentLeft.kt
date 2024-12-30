9abdef8b4ee0e04884d94c990ca008b54e29936e
TAL <tal@YmyMacBook-Pro.local>
2024-12-30 21:01:40 +0800

    fragment生命周期

--- app/src/main/java/com/example/applicationdemo5/FragmentLeft.kt
+++ app/src/main/java/com/example/applicationdemo5/FragmentLeft.kt
@@ -1,5 +1,6 @@
 package com.example.applicationdemo5
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -11,23 +12,76 @@ import androidx.fragment.app.Fragment
 class FragmentLeft : Fragment() {
 
     private lateinit var button: Button
+    private lateinit var view: View
+    
+    companion object {
+        const val TAG = "leftFragment"
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "onAttach: ")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate: ")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_left, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        view = inflater.inflate(R.layout.fragment_left, container, false)
         button = view.findViewById(R.id.button_fragment)
         button.setOnClickListener {
             //通知Activity加载右侧的fragment
-            Log.d("FragmentLeft", "点到了button")
+            Log.d(TAG, "button_fragment: ")
             (activity as? FragmentActivity)?.loadRightFragment()
         }
+        Log.d(TAG, "onCreateView: ")
+        return view
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Log.d(TAG, "onActivityCreated: ")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "onDestroyView: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(TAG, "onDetach: ")
+    }
+    
 }
