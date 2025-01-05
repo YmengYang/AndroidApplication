ce3a4e39cf014ca65c0a75231a74a88ee4e78737
YmengYang <3421849869@qq.com>
2025-01-05 22:52:41 +0800

    匿名函数

--- app/src/main/java/com/example/kotlin/AnonymousFun/AnonymousFun01.kt
+++ app/src/main/java/com/example/kotlin/AnonymousFun/AnonymousFun01.kt
@@ -1,9 +1,21 @@
 package com.example.kotlin.AnonymousFun
 
+//StringBuilder.()
+//在函数类型的前面加上ClassName. 就表示这个函数类型是定义在哪个类当中的。
+fun StringBuilder.build(block: StringBuilder.() -> Unit): StringBuilder {
+    block()
+    return this
+}
+
 fun main() {
-    val total = "ymy".count()
-    "ymy".count({letter ->
-        letter == 'y'
-    })
+    val list = listOf("Apple", "Orange")
+    val result = StringBuilder().build {
+        append("Start eating.\n")
+        for (fruit in list) {
+            append(fruit).append("\n")
+        }
+        append("Ate all")
+    }
+    println(result.toString())
 
 }
